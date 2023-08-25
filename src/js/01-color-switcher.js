005a403b5010b9bd17b1031f3bdb47544917dbcd
Emilia Wenta <emiliakielinska96@gmail.com>
2023-08-25 15:24:32 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/js/01-color-switcher.js 	modified:   src/js/02-timer.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+startBtn = buttons[0];
+stopBtn = buttons[1];
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const getColor = getRandomHexColor();
+    body.style.background = getColor;
+    localStorage.setItem('bodyRandomHexColor', getColor);
+    startBtn.setAttribute('disabled', 'disabled');
+    stopBtn.removeAttribute('disabled');
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  const currentColor = localStorage.getItem('bodyRandomHexColor');
+  clearInterval(timerId);
+  body.style.background = currentColor;
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled');
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,101 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+todayDate = new Date().getTime();
+const timerBox = document.querySelector('.timer');
+timerBox.style.display = 'flex';
+timerBox.style.gap = '15px';
+const spanWithValue = document.querySelectorAll('.value');
+
+const timerBoxFieldsArray = () => {
+  const array = [];
+  const item = document.querySelectorAll('.field');
+  array.push(...item);
+
+  for (const item of array) {
+    item.style.display = 'flex';
+    item.style.flexDirection = 'column';
+    item.firstElementChild.style.display = 'flex';
+    item.firstElementChild.style.fontSize = '30px';
+    item.firstElementChild.style.lineHeight = '20px';
+    item.firstElementChild.style.paddingTop = ' 15px';
+    item.firstElementChild.style.paddingBottom = ' 5px';
+    item.firstElementChild.style.justifyContent = 'center';
+    item.lastElementChild.style.textTransform = 'uppercase';
+  }
+};
+
+timerBoxFieldsArray();
+
+const startBtn = document.querySelector('button');
+startBtn.setAttribute('disabled', 'disabled');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const date = new Date();
+
+    if (selectedDates[0] > date) {
+      localStorage.setItem('chosedDate', `${selectedDates[0].getTime()}`);
+      startBtn.removeAttribute('disabled');
+    } else {
+      startBtn.setAttribute('disabled', 'disabled');
+      alert('Please choose a date in the future');
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function generateTime() {
+  const ms = localStorage.getItem('chosedDate') - todayDate;
+
+  localStorage.setItem('convertMS', JSON.stringify(convertMs(ms)));
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  const value = JSON.parse(localStorage.getItem('convertMS'));
+
+  function addLeadingZero(value) {
+    for (const key in value) {
+      if (value[key] < 10) {
+        value[key] = value[key].toString().padStart(2, '0');
+      } else {
+        continue;
+      }
+    }
+    return value;
+  }
+
+  addLeadingZero(value);
+
+  const valueArray = Object.values(value);
+
+  for (let i = 0; i < spanWithValue.length; i++) {
+    spanWithValue[i].textContent = valueArray[i];
+  }
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(generateTime(), 1000);
+});
